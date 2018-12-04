909590cf7cb10303d6604f4a9d25322564623939
kitzeller <kit.zeller@gmail.com>
2018-12-04 18:31:17 -0500

    Chips

--- app/src/main/java/wpi/jtkaplan/teamup/ClassFragment.java
+++ app/src/main/java/wpi/jtkaplan/teamup/ClassFragment.java
@@ -1,16 +1,26 @@
 package wpi.jtkaplan.teamup;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
+import com.hootsuite.nachos.ChipConfiguration;
 import com.hootsuite.nachos.NachoTextView;
+import com.hootsuite.nachos.chip.ChipSpan;
+import com.hootsuite.nachos.chip.ChipSpanChipCreator;
 import com.hootsuite.nachos.terminator.ChipTerminatorHandler;
+import com.hootsuite.nachos.tokenizer.SpanChipTokenizer;
 
 import java.util.ArrayList;
+
+import wpi.jtkaplan.teamup.model.Class;
 
 /**
  * Class for creating a class
@@ -18,14 +28,49 @@ import java.util.ArrayList;
 
 public class ClassFragment extends Fragment{
 
+    private ArrayList<String> tags;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v  = inflater.inflate(R.layout.fragment_create_class, container, false);
 
+        tags = new ArrayList<>();
+        tags.add("Java");
+        tags.add("Python");
+        tags.add("OOP");
+        tags.add("Extra");
+        tags.add("Thing");
+
         NachoTextView nv = v.findViewById(R.id.nacho_text_view);
+        nv.setChipTokenizer(new SpanChipTokenizer<>(getActivity(), new ChipSpanChipCreator() {
+            @Override
+            public ChipSpan createChip(@NonNull Context context, @NonNull CharSequence text, Object data) {
+                if (tags.contains(text.toString())) {
+                    return new ChipSpan(context, text, ContextCompat.getDrawable(getActivity(), R.mipmap.ic_launcher), data);
+                }
+
+                return new ChipSpan(context, "null", ContextCompat.getDrawable(getActivity(), R.mipmap.ic_launcher), data);
+            }
+
+            @Override
+            public void configureChip(@NonNull ChipSpan chip, @NonNull ChipConfiguration chipConfiguration) {
+                if (!chip.getText().toString().equals("null")){
+                    super.configureChip(chip, chipConfiguration);
+                }
+            }
+        }, ChipSpan.class));
+
+
+
+
+
         nv.addChipTerminator('\n', ChipTerminatorHandler.BEHAVIOR_CHIPIFY_ALL);
         nv.enableEditChipOnTouch(true, true);
+
+        String[] suggestions = new String[]{"Tortilla Chips", "Melted Cheese", "Salsa", "Guacamole", "Mexico", "Jalapeno"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line, suggestions);
+        nv.setAdapter(adapter);
 
         ArrayList<String> tags = (ArrayList<String>) nv.getChipValues();
 
